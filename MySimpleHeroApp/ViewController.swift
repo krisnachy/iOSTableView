89f954ab1973849f11d6389fcb94ca402d462fa8
krisnachy <cahyayuana@gmail.com>
2023-06-09 13:54:26 +0700

    Initial Commit

---
+++ MySimpleHeroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySimpleHeroApp
+//
+//  Created by Cahyo on 09/06/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
